25d2242241c0cb05cb823f78cf28d2ef866a1fb6
okoemm <okoemm@users.noreply.github.com>
2015-05-05 21:25:12 +0200

    Initial release

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # aes-wipe
-Wipes HDDs using OpenSSL, requires AES-NI capable CPU
+This script wipes HDDs using OpenSSL. It takes advantage of the AES-NI feature which makes wiping a lot faster than conventional programs (e.g. [Darik's Boot and Nuke](http://dban.org))
+
+### Usage
+
+Simply run the script and append the device name:
+
+    $ ./aes-wipe.sh /dev/sda
+    
+#####Disclaimer
+I am not responsible for any data loss or damage caused by this script. Use it at your own risk.
---
+++ aes-wipe.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+$device = $1
+
+echo '***********************************************'
+echo '/                   aes-wipe                  /'
+echo '***********************************************'
+echo 'This script will wipe the entire HDD you selected.'
+echo 'USE AT YOUR OWN RISK!'
+echo ''
+echo 'Do you want to proceed? (y/n):'
+read eingabe
+if  [ "$eingabe" == "y" ]
+	then
+		echo 'Wiping in progress...'
+		
+		echo 'Pass 1 - Writing zeros'
+		dd if=/dev/zero of=$device bs=4M 2> /dev/null
+		
+		echo 'Pass 2 - Writing ones'
+		dd if=/dev/zero count=100 bs=1 | tr "\000" "\377" > $device 2> /dev/null
+		
+		echo 'Pass 3 - Writing random data'
+		openssl enc -aes-256-ctr -pass pass:"$(dd if=/dev/urandom bs=128 count=1 2>/dev/null | base64)" -nosalt < /dev/zero > $device 2> /dev/null
+		
+		echo 'Pass 4 - Writing random data'
+		openssl enc -aes-256-ctr -pass pass:"$(dd if=/dev/urandom bs=128 count=1 2>/dev/null | base64)" -nosalt < /dev/zero > $device 2> /dev/null
+		
+		echo 'Pass 5 - Writing random data'
+		openssl enc -aes-256-ctr -pass pass:"$(dd if=/dev/urandom bs=128 count=1 2>/dev/null | base64)" -nosalt < /dev/zero > $device 2> /dev/null
+		
+		
+		echo 'Finished at: '
+		echo date
+		
+else
+	echo 'Process aborted.'
+fi
+exit
